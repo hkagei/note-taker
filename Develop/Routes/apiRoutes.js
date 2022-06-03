db931869da1ff01d40191283fac61e6a2f202177
hkagei <k_dawg_1022@hotmail.com>
2022-06-02 20:33:00 -0600

    secondary commit, wrote out htmlRoutes.js

---
+++ Develop/Routes/apiRoutes.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require("path");
+const router = require('express').Router();
+
+router.get('/notes',);
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const htmlRoutes = require('./Routes/htmlRoutes');
+const apiRoutes = require('./Routes/apiRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
