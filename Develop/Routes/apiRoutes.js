64a98754eeae5b469e4e09d7e23607305ceb3dbd
hkagei <k_dawg_1022@hotmail.com>
2022-06-07 18:49:43 -0600

    final commit before heroku deployment

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,7 +1,35 @@
 const fs = require('fs');
 const path = require("path");
 const router = require('express').Router();
+const notes = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
-router.get('/notes',);
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+    
+});
+
+router.post('/notes', (req, res) => {
+    let newNote = req.body;
+    newNote.id = uuidv4();
+        notes.push(newNote);
+    fs.writeFile(path.resolve(__dirname, "../db/db.json"), JSON.stringify(notes), function(error){
+        if (error) {
+                console.error(error);
+                res.status(500).send("An error occurred, please try again");
+                }
+                else {
+                    res.json(newNote)
+                }
+    })
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    
+    notes.splice(req.params.id, 1);
+    res.json(noteId);
+})
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const htmlRoutes = require('./Routes/htmlRoutes');
 const apiRoutes = require('./Routes/apiRoutes');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,8 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
